d39c7cfb7835507208e69889d7b251745eeda120
Steve Klabnik <steve@steveklabnik.com>
2015-08-17 15:56:24 -0400

    Small grammar fixes in documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
-//! Fancy pure Rust crypto and hash library that main reason to exist is my own self-teaching
-//! about cryptography. Other reason to create this ("We have rust-crypto, why the hell new
-//! library?") is that `rust-crypto` was (at the time of creation) big pile of… mess. To be honest
+//! Fancy pure Rust crypto and hash library.
+//! 
+//! The main reason this exists is so I can learn about cryptography.
+//! 
+//! If you're asking "We have rust-crypto, why the hell do we need a new
+//! library?"), `rust-crypto` was (at the time of Octavio's creation) big pile of… mess. To be honest
 //! I doesn't hate `rust-crypto` but I wanted something that will be organized in fancy way, not
-//! new OpenSSL (but wwritten in Rust, so probably less buggy).
+//! another OpenSSL (but written in Rust, so probably less buggy).
 //!
-//! Other reason is that I want Octavo to became highly configurable via Cargo features. Each
-//! cipher, hash family, MAC, etc. should be behind feature.
+//! Anther reason is that I want Octavo to became highly configurable via Cargo features. Each
+//! cipher, hash family, MAC, etc. should be behind a feature.
 //!
 //! Oh… one more thing. I want to create C wrapper for Octavo (maybe even OpenSSL compatible), so
-//! you could use this beautiful peace of crap.
+//! that you could use this beautiful peace of crap.
 //!
 //! Happy coding!
 //!
