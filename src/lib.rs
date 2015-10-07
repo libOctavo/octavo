ab0713ab2e2c395fbc0cdd62e7594ad083468bde
Łukasz Jan Niemier <lukasz@niemier.pl>
2015-10-07 10:26:10 +0200

    fix(): fix typos

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! The main reason this exists is so I can learn about cryptography.
 //!
 //! If you're asking "We have rust-crypto, why the hell do we need a new
-//! library?"), `rust-crypto` was (at the time of Octavio's creation) big pile of… mess. To be honest
+//! library?"), `rust-crypto` was (at the time of Octavo's creation) big pile of… mess. To be honest
 //! I doesn't hate `rust-crypto` but I wanted something that will be organized in fancy way, not
 //! another OpenSSL (but written in Rust, so probably less buggy).
 //!
